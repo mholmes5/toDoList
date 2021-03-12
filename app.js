4c0bc7c2dc992c4e9f64a67e42d125b809f1f1d8
Michael Holmes <mholmes5@asu.edu>
2021-03-11 20:37:27 -0600

    Updates for new list items

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.set('view engine', 'ejs');
 
+let tasks =["Buy Food","Cook Food","Eat Food"];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 /*app.get("/", (req,res)=>{
   //res.sendFile(__dirname +"/index.html");
-  var today = new Date
-  var dayOfWeek = today.toLocaleString("en-US", {weekday: "long"});
+  let today = new Date
+  let dayOfWeek = today.toLocaleString("en-US", {weekday: "long"});
   if(today.getDay()===6 || today.getDay()===0){
     res.send("<h1>Yay it is the weekend</h1><p>Today is " +dayOfWeek +".</p>");
   }else{
@@ -20,10 +23,26 @@ app.set('view engine', 'ejs');
 });*/
 
 app.get("/", (req,res)=>{
-  var today = new Date();
-  var dayOfWeek = today.toLocaleString("en-US",{weekday: "long"});
-  res.render('list', {day:dayOfWeek});
+  let today = new Date();
+
+  let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+
+  let day= today.toLocaleDateString("en-US",options);
+  res.render('list', {day:day, newListItems:tasks});
 });
+
+
+app.post("/",(req,res)=>{
+  tasks.push(req.body.newTask);
+  res.redirect("/");
+
+});
+
 
 app.listen(3000, ()=>{
   console.log("Server started on port 3000");
